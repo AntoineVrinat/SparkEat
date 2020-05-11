ca6b509c11e1fb8c9bd36953d290f47a1bde32bd
AntoineVrinat <antoine.vrinat.91@gmail.com>
2020-05-11 12:20:51 +0200

    Menu

--- src/main/java/model/Menu.java
+++ src/main/java/model/Menu.java
@@ -1,5 +1,50 @@
 package model;
 
 public class Menu {
+	
+	protected double prix;
+	protected String plat;
+	protected String boisson;
+	
+	protected Menu(double prix, String plat, String boisson) {
+		this.prix = prix;
+		this.plat = plat;
+		this.boisson = boisson;
+	}
 
+	protected Menu() {
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public String getPlat() {
+		return plat;
+	}
+
+	public void setPlat(String plat) {
+		this.plat = plat;
+	}
+
+	public String getBoisson() {
+		return boisson;
+	}
+
+	public void setBoisson(String boisson) {
+		this.boisson = boisson;
+	}
+
+	@Override
+	public String toString() {
+		return "Menu [prix=" + prix + ", plat=" + plat + ", boisson=" + boisson + "]";
+	}
+
+	
+	
+	
 }
