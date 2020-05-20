4e2bf6c32aaf41c86e5ae9d8546a17dc2a88ba81
Lalie <modestecoralie@orange.fr>
2020-05-20 14:18:33 +0200

    Modif appModule

--- SparkEatAngular/src/app/app.module.ts
+++ SparkEatAngular/src/app/app.module.ts
@@ -1,26 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CommandeComponent } from './commande/commande.component';
-import { ClientComponent } from './client/client.component';
 import { HomeComponent } from './home/home.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { ChoixRestaurantComponent } from './choix-restaurant/choix-restaurant.component';
 
+
+const routes: Routes = [
+  { path: 'accueil', component: HomeComponent },
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'choixRestaurant', component: ChoixRestaurantComponent },
+//  { path: 'choixCuisine', component: ChoixCuisinesComponent },
+//  { path: 'choixMenu', component: ChoixMenuComponent },
+  { path: 'commande', component: CommandeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+];
+
+
 @NgModule({
   declarations: [
     AppComponent,
     CommandeComponent,
-    ClientComponent,
     HomeComponent,
     InscriptionComponent,
     ConnexionComponent,
     ChoixRestaurantComponent
   ],
-  imports: [
-    BrowserModule
+  imports: 
+  [
+    BrowserModule, FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
